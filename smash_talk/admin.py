92e4758e3739f5dd3f5f440dba43c982542fefbd
RousanChandra <syahrousan@gmail.com>
2025-10-24 10:45:20 +0700

    a

--- smash_talk/admin.py
+++ smash_talk/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Comment, UserProfile
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'is_published', 'created_at')
+    list_filter = ('category', 'is_published', 'created_at', 'author')
+    search_fields = ('title', 'content', 'author__username')
+    prepopulated_fields = {"slug": ("title",)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('content', 'author__username')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    list_filter = ('role',)
